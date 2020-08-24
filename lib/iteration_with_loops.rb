cfa4841f99a7193ca39a4757a3e4c067fbe76a9d
Brandon Fonnemann <branjosef@gmail.com>
2020-08-24 18:39:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,6 +6,10 @@ def find_even_values(src)
   row_index = 0 
   while row_index < src.count do
     element_index = 0 
-    
+    while element_index < src[row_index].count do 
+      src[row_index].select {|number| number.even?}
+      element_index += 1
+    end
+    row_index += 1
 
 end
